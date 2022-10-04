635cf27175f62143f213a6360cc37673a94617cf
Livia <liviafausto03@gmail.com>
2022-10-03 21:36:03 -0300

    ex de lista estatica funcionando corretamente

--- exercicios/ex lista estatica/produtos.c
+++ exercicios/ex lista estatica/produtos.c
@@ -2,8 +2,6 @@
 #include <stdio.h>
 #include <string.h>
 
-static marcador pesquisa_posicao(lista *l, chave codigo_produto); //função que pesquisa o codigo de um produto e retorna sua posição como um marcador
-
 int criar(lista *l){
     l->ultimo = -1;
 }
@@ -25,12 +23,12 @@ int cheia(lista *l){
 int inserir(lista *l, produto novo_produto){
     if(cheia(l)){
         printf("Lista cheia.\n\n");
-        return ERRO;
+        return ERRO_CHEIA;
     }
 
     if(pesquisa_posicao(l, novo_produto.codigo) >= 0){
         printf("Produto ja existe.\n\n");
-        return ERRO;
+        return JA_EXISTE;
     }
 
     l->ultimo++;
@@ -39,26 +37,60 @@ int inserir(lista *l, produto novo_produto){
     return SUCESSO;
 }
 
+int pesquisar(lista *l, produto *pesquisar_produto){
+    if(vazia(l)){
+        printf("Lista vazia.\n");
+    }
+    marcador posicao_produto = pesquisa_posicao(l, pesquisar_produto->codigo);
+
+    if(posicao_produto < 0){
+        printf("Esse produto nao existe\n");
+        return NAO_ENCONTROU;
+    }
+
+    strcpy(pesquisar_produto->nome, l->pecas[posicao_produto].nome);
+    pesquisar_produto->quantidade = l->pecas[posicao_produto].quantidade;
+    pesquisar_produto->peso = l->pecas[posicao_produto].peso;
+
+    return SUCESSO;
+}
+
+int alterar(lista *l, chave codigo_antigo){
+    if(vazia(l)){
+        printf("Lista vazia.\n");
+        return ERRO_VAZIA;
+    }
+    marcador alterar_posicao = pesquisa_posicao(l, codigo_antigo);
+
+    if(alterar_posicao < 0){
+        printf("Esse produto nao existe.\n");
+        return NAO_ENCONTROU;
+    }
+
+    escolher(&l->pecas[alterar_posicao]);
+
+    return SUCESSO;
+}
+
 int remover(lista *l, chave codigo_produto){
     if(vazia(l)){
         printf("Lista vazia");
-        return ERRO;
+        return ERRO_VAZIA;
     }
 
     marcador remover_posicao = pesquisa_posicao(l, codigo_produto);
     
-    if(remover_posicao >= 0){
-
-        for(int i=remover_posicao; i < l->ultimo; i++){
-            l->pecas[i] = l->pecas[i+1];
-        }
-
-        l->ultimo--;
-        return SUCESSO;
-
-    } else {
-        return ERRO;
+    if(remover_posicao < 0){
+        printf("Esse item nao existe.\n");
+        return NAO_ENCONTROU;
     }
+
+    for(int i=remover_posicao; i < l->ultimo; i++){
+        l->pecas[i] = l->pecas[i+1];
+    }
+
+    l->ultimo--;
+    return SUCESSO;
 
 }
 
@@ -71,32 +103,51 @@ static marcador pesquisa_posicao(lista *l, chave codigo_produto){
             return posicao;
     }
 
-    return ERRO;
-
-}
-produto pesquisar(lista *l, chave codigo){
-    marcador produto = pesquisa_posicao(l, codigo);
-
-    if(produto >= 0){
-        return l->pecas[produto];
-    }
-    else {
-
-    }
+    return NAO_ENCONTROU;
 
 }
 
-int alterar(lista *l, chave codigo_antigo){
-    if(vazia(l))
-        return ERRO;
+static void escolher(produto *antigo){
+    int opcao=0;
 
-    marcador posicao = pesquisa_posicao(l, codigo_antigo);
+    while(opcao != 1 && opcao != 2 && opcao != 3 && opcao != 4){
+        printf("\nO que voce deseja alterar?\n\n");
+        printf("1- Trocar codigo do produto\n"); 
+        printf("2- Trocar nome do produto\n");
+        printf("3- Trocar quantidade do produto\n"); 
+        printf("4- Trocar peso do produto\n\n");
 
-    if(posicao >= 0){
-        l->pecas[posicao] = escolher(l->pecas[posicao]);
-        return SUCESSO;
+        scanf("%d", &opcao);
 
-    } else {
-        return ERRO;
+        switch (opcao){
+        case 1:
+            printf("Digite o novo codigo do produto: "); 
+            scanf("%d", &antigo->codigo);
+
+            break;
+        
+        case 2:
+            printf("Digite o novo nome do produto: "); 
+            scanf("%s", antigo->nome);
+
+            break;
+
+        case 3:
+            printf("Digite a nova quantidade do produto: "); 
+            scanf("%d", &antigo->quantidade);
+
+            break;
+
+        case 4:
+            printf("Digite o novo peso do produto: "); 
+            scanf("%lf", &antigo->peso);
+
+            break;
+        
+        default:
+            printf("Insira uma opcao valida para continuar.\n");
+            break;
+        }
     }
+
 }
--- exercicios/ex lista estatica/produtos.h
+++ exercicios/ex lista estatica/produtos.h
@@ -3,9 +3,11 @@
 
         #define TAM_MAX 50
 
+        #define JA_EXISTE -3
+        #define NAO_ENCONTROU -2
+        #define ERRO_VAZIA -1
+        #define ERRO_CHEIA 0
         #define SUCESSO 1
-        #define LISTA_CHEIA 0
-        #define ERRO -2
 
         typedef int chave;
         typedef int marcador;
@@ -27,16 +29,17 @@
         int vazia(lista *l);
         int cheia(lista *l);
         int inserir(lista *l, produto novo_produto);
-        int remover(lista *l, chave codigo_produto);
-        produto pesquisar(lista *l, chave codigo_produto);
+        int pesquisar(lista *l, produto *pesquisar_produto);
         int alterar(lista *l, chave codigo_antigo);
+        int remover(lista *l, chave codigo_produto);
+        static marcador pesquisa_posicao(lista *l, chave codigo_produto);
+        static void escolher(produto *antigo);
 
         //menu.c
+        int menu();
         produto ler_produto();
         chave ler_codigo();
         void imprimir_lista(lista *l);
         void imprimir_produto(produto pesquisado);
-        produto escolher(produto antigo);
-        produto copy(produto antigo, produto modificado, int escolha);
 
 #endif
