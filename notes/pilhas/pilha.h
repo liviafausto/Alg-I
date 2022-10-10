3c94ab41f398339fa8e9323ee926253cacb51b03
Livia <liviafausto03@gmail.com>
2022-10-10 16:38:09 -0300

    primeira versao de pilha.h adicionada

---
+++ notes/pilhas/pilha.h
@@ -0,0 +1,46 @@
+#ifndef PILHA_H
+    #define PILHA_H
+
+        #define JA_EXISTE -3
+        #define NAO_ENCONTROU -2
+        #define ERRO_VAZIA -1
+        #define ERRO_CHEIA 0
+        #define SUCESSO 1
+
+        typedef int chave;
+
+        typedef struct elemento {
+            chave codigo;
+            char nome[50];
+        } elemento;
+
+        typedef struct no *apontador;
+
+        typedef struct no {
+            elemento item;
+            apontador proximo;
+        } no;
+
+        typedef struct {
+            apontador topo;
+        } pilha;
+
+        int criar(pilha *p);
+        int vazia(pilha *p);
+        int cheia(pilha *p);
+
+        int push(pilha *p, elemento item); //empilhar
+        int pop(pilha *p); //desempilhar
+        no topo(pilha *p);
+        int contar(pilha *p);
+        //inverte(pilha *p);
+
+        void imprime(pilha *p);
+
+#endif
+
+/*
+especialização de lista
+inserções e remoções sao feitas no topo
+last-in / first-out (LIFO)
+*/
