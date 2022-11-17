b4dd8640020fab63c87ec7eac9ab97abf2cc1a97
Livia <liviafausto03@gmail.com>
2022-11-17 19:44:16 -0300

    heap: adicionando arquivos

---
+++ notes/heap/heap.c
@@ -0,0 +1,20 @@
+#include "heap.h"
+#include <stdio.h>
+
+void bubble_up(int* heap, int no){
+
+    //condicao de parada
+
+    int pai = no/2;
+}
+
+void inserir(int *heap){
+
+    int ultimo = (++heap[0]);
+    //heap[ultimo] = chave;
+
+}
+
+int frente(int* heap){
+    return heap[1];
+}
---
+++ notes/heap/heap.h
@@ -0,0 +1,7 @@
+#ifndef HEAP_H
+    #define HEAP_H
+
+        #define MAX 129
+
+
+#endif
