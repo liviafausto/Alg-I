73fe44256b4db81d23b75bfd18ebda2ad945faf1
Livia <liviafausto03@gmail.com>
2022-12-10 20:32:00 -0300

    AVL: função de rotação para a esquerda adicionada

--- notes/arvores/arvores-balanceadas/avl.c
+++ notes/arvores/arvores-balanceadas/avl.c
@@ -6,6 +6,9 @@
 
 //AVL: nome vem dos seus criadores - Adelson-Velsky e Landis
 
+static int max(int a, int b);
+static void rotacao_esquerda(arvore *avl);
+
 int criar(arvore *avl){
     //ideal seria limpar caso houvesse algo
     *avl = NULL;
@@ -32,3 +35,29 @@ int checar_fatorBalanceamento(arvore *avl){
 int inserir(arvore *avl, elemento folha);
 elemento pesquisar(arvore *avl, chave codigo);
 int remover(arvore *avl, chave codigo);
+
+static int max(int a, int b){
+    if(a > b){
+        return a;
+    } else {
+        return b;
+    }
+}
+
+
+static void rotacao_esquerda(arvore *avl){
+    apontador aux, B;
+
+    // filho à esquerda da raiz
+    aux = (*avl)->esq;
+
+    // filho à esquerda à direita da raiz
+    B = aux->dir;
+
+    //a sub-arv muda de pai pela antiga raiz
+    (*avl)->esq = B;
+    //aux passa a ser a raiz
+    aux = (*avl);
+
+    (*avl)->altura = max(retornar_altura(&(*avl)->esq), retornar_altura(&(*avl)->dir + 1));
+}
